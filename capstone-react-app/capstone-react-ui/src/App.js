541df03a91a4787c4bff7b9a03cf00d84d0e1dfa
Kathryn Rowzee <kathryntrowzee@gmail.com>
2021-07-02 13:24:15 -0500

    Profile page

--- capstone-react-app/capstone-react-ui/src/App.js
+++ capstone-react-app/capstone-react-ui/src/App.js
@@ -1,7 +1,57 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginContext from "./contexts/LoginContext";
+import Fail from "./components/Fail";
+import MainPage from "./MainPage";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Profile from "./components/Profile/Profile";
+
+
 function App() {
-  return (
-    <div className="App">
-      Vacation App
+  const [credentials, setCredentials] = useState({
+    username: null,
+    jwt: null
+  });
+
+  const afterAuth = token => {
+    const firstDot = token.indexOf(".");
+    const secondDot = token.indexOf(".", firstDot + 1);
+    const jwtBody = token.substring(firstDot + 1, secondDot);
+    const body = JSON.parse(atob(jwtBody));
+    setCredentials({
+      username: body.sub,
+      jwt: token
+    });
+  };
+
+  const logout = () => setCredentials({ username: null, jwt: null })
+
+  return (<div className="container">
+    <LoginContext.Provider value={{ ...credentials, afterAuth, logout }}>
+        <Router>
+          <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/failure">
+            <Fail />
+          </Route>
+          <Route path="/">
+            <MainPage />
+          </Route>
+            <Route>
+              <Fail />
+            </Route>
+          </Switch>
+        </Router>
+      </LoginContext.Provider>
     </div>
   );
 }
