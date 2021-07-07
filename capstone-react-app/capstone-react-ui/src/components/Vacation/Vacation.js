39a834b757774e149e870116de9b755aba66e4ce
Robb Entenmann <robbentenmann@hotmail.com>
2021-07-07 11:16:15 -0500

    Updated Vacation/App

--- capstone-react-app/capstone-react-ui/src/components/Vacation/Vacation.js
+++ capstone-react-app/capstone-react-ui/src/components/Vacation/Vacation.js
@@ -1,63 +1,72 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useParams } from "react";
 import ReactMapGL, {Marker} from 'react-map-gl';
 import { useHistory } from "react-router";
 import { findAllLocations } from "../../services/locations";
+import { findById } from "../../services/vacations";
 import Icon from "../Location/city-pin";
 import PolylineOverlay from "./PolylineOverlay";
 
 function Vacation() {
 
-  const [locations, setLocations] = useState([]);
-  const history = useHistory();
+    const [locations, setLocations] = useState([]);
+    const [vacation, setVacation] = useState([]);
+    const history = useHistory();
+    const { id } = useParams();
 
-  useEffect(() => {
-      findAllLocations()
-        .then(setLocations)
-        .catch(() => history.push("/failure"));
-  }, [history]);
+    useEffect(() => {
+        findById(id)
+            .then(setVacation)
+            .catch(() => history.push("/failure"));
+    }, [id, history]);
 
-  const [viewport, setViewport] = useState({
-    latitude: 38.8,
-    longitude: -96.4,
-    zoom: 3.5
-  });
+    useEffect(() => {
+        findAllLocations()
+            .then(setLocations)
+            .catch(() => history.push("/failure"));
+    }, [history]);
 
-  const onMapClick = description => {
-    history.push(`/location/${description}`);
-  };
+    const [viewport, setViewport] = useState({
+        latitude: 38.8,
+        longitude: -96.4,
+        zoom: 3.5
+    });
 
-  const markers = () => locations.map(
-    location => (
-      <Marker key={location.description} longitude={location.longitude} latitude={location.latitude} >
-        <Icon onClick={() => onMapClick(location.description)}/>
-      </Marker>
-    )
-  );
+    const onMapClick = description => {
+        history.push(`/location/${description}`);
+    };
 
-  const points = () => locations.map(
-    location => [location.longitude, location.latitude]   
-  );
+    const markers = () => locations.map(
+        location => (
+        <Marker key={location.description} longitude={location.longitude} latitude={location.latitude} >
+            <Icon onClick={() => onMapClick(location.description)}/>
+        </Marker>
+        )
+    );
 
-  return (<div className="row align-items-center">
-    <div className="container">
-        <h3>View your vacation</h3>
-    </div>
-    <div className="mapbox-react">
-      <ReactMapGL
-        {...viewport}
-        width="1300px"
-        height="500px"
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-        onViewportChange={nextViewport => setViewport(nextViewport)}
-        mapboxApiAccessToken={`pk.eyJ1Ijoicm9iYmU4NyIsImEiOiJja3FtajIzY2owODFzMnZtem02OTJndjF3In0.xekIFGpFViXp6WPMgmSyhg`}>
-        <PolylineOverlay points={points()} />
-        {markers()}
-      </ReactMapGL>
-    </div>
-    <div className="container">
-        <h3>Vacation Comments</h3>
-    </div>
-  </div>);
+    const points = () => locations.map(
+        location => [location.longitude, location.latitude]   
+    );
+
+    return (<div className="row align-items-center">
+        <div className="container">
+            <h3>View your vacation</h3>
+        </div>
+        <div className="mapbox-react">
+        <ReactMapGL
+            {...viewport}
+            width="1300px"
+            height="500px"
+            mapStyle="mapbox://styles/mapbox/streets-v11"
+            onViewportChange={nextViewport => setViewport(nextViewport)}
+            mapboxApiAccessToken={`pk.eyJ1Ijoicm9iYmU4NyIsImEiOiJja3FtajIzY2owODFzMnZtem02OTJndjF3In0.xekIFGpFViXp6WPMgmSyhg`}>
+            <PolylineOverlay points={points()} />
+            {markers()}
+        </ReactMapGL>
+        </div>
+        <div className="container">
+            <h3>Vacation Comments</h3>
+        </div>
+    </div>);
 }
 
 export default Vacation;
--- capstone-react-app/capstone-react-ui/src/services/vacations.js
+++ capstone-react-app/capstone-react-ui/src/services/vacations.js
@@ -1,5 +1,13 @@
 const url = "http://localhost:8080/api/vacation";
 
+export async function findById(vacationId) {
+    const response = await fetch(`${url}/vid/${vacationId}`);
+    if (response.status === 200) {
+        return await response.json();
+    }
+    return Promise.reject("not 200 OK");
+}
+
 export async function findVacationsByUser(userId) {
     const response = await fetch(`${url}/uid/${userId}`);
     if (response.status === 200) {
