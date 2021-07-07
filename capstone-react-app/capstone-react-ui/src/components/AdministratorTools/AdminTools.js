bdff72b4290cb063d6ec8f24d5312c4f693a2184
Kathryn Rowzee <kathryntrowzee@gmail.com>
2021-07-07 09:44:52 -0500

    admin formatting

--- capstone-react-app/capstone-react-ui/src/components/AdministratorTools/AdminTools.js
+++ capstone-react-app/capstone-react-ui/src/components/AdministratorTools/AdminTools.js
@@ -22,7 +22,7 @@ function AdminTools(){
             .catch(() => history.push("./failure"));
     }, [history]);
 
-    return(<div>
+    return(<div className="container white">
         <div className="col d-flex justify-content-end">
           <Link to="/admintools/location/add" className={`btn btn-primary m-1`}>Add a Location</Link>
         </div>
@@ -31,12 +31,12 @@ function AdminTools(){
             <div className="col-md-6">
                 <h1 className="col">Users</h1>
                 <div className="row grid-header">
-                    <div className="col">First Name</div>
-                    <div className="col">Last Name</div>
-                    <div className="col">Usename</div>
+                    <div className="col title">First Name</div>
+                    <div className="col title">Last Name</div>
+                    <div className="col title">Usename</div>
                     <div className="col"></div>
                 </div>
-                {users && users.map(u => <div key={u.userId} className="row mt-2">
+                {users && users.map(u => <div key={u.userId} className="row mb-2 py-1 alt-colors">
                     <div className="col">{u.firstName}</div>
                     <div className="col">{u.lastName}</div>
                     <div className="col">{u.username}</div>
@@ -45,15 +45,16 @@ function AdminTools(){
                     <Link to={`/admintools/user/edit/${u.userId}`} className="btn btn-info">Edit</Link></div>
                 </div>)}
             </div>
-
-            <div className="col-md-6">
+            <div className="col-1"></div>
+            <div className="col-5">
                 <h1 className="col">Location</h1>
                 <div className="row grid-header">
-                    <div className="col">Description</div>
+                    <div className="col title">Description</div>
                     <div className="col"></div>
                 </div>
-                {locations && locations.map(l => <div key={l.locationId} className="row mt-2">
+                {locations && locations.map(l => <div key={l.locationId} className="row mb-2 py-1 alt-colors">
                     <div className="col">{l.description}</div>
+                    <div className="col-5"></div>
                     <div className="col">
                     <Link to={`/admintools/location/delete/${l.locationId}`} className="btn btn-danger me-2">Delete</Link>
                     <Link to={`/admintools/location/edit/${l.locationId}`} className="btn btn-info">Edit</Link></div>
